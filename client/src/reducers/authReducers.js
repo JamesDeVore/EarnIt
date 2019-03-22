66cc7dd19265573bd04af19a13eb53b7c3e6c41c
James DeVore <jdevore4592@gmail.com>
2019-03-22 13:57:38 -0400

    redux is set up

---
+++ client/src/reducers/authReducers.js
@@ -0,0 +1,25 @@
+import {SET_CURRENT_USER, USER_LOADING} from "../actions/types"
+
+const isEmpty = require("is-empty")
+
+const initialState = {
+  isAuthenticated:false,
+  user:{},
+  loading:false
+}
+
+export default function(state = initialState, action){
+  let {type, payload} = action
+  switch(type){
+    case SET_CURRENT_USER:
+      return{
+        ...state,
+        isAuthenticated:!isEmpty(payload),
+        user:payload
+      }
+    case USER_LOADING:
+      return {...state, loading:true}
+    default:
+      return state
+  }
+}
